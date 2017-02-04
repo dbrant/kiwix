f786723ad45d563c54ef3d9574eed4c99d8e7d25
Dmitry Brant <dbrant@wikimedia.org>
2017-02-03 23:33:19 -0500

    No longer add stub HTML.

--- src/common/kiwix/reader.cpp
+++ src/common/kiwix/reader.cpp
@@ -498,13 +498,6 @@ namespace kiwix {
 	  content = string(article.getData().data(), article.getArticleSize());
 	}
 
-	/* Try to set a stub HTML header/footer if necesssary */
-	if (contentType.find("text/html") != string::npos && 
-	    content.find("<body") == std::string::npos &&
-	    content.find("<BODY") == std::string::npos) {
-	  content = "<html><head><title>" + article.getTitle() + "</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head><body>" + content + "</body></html>";
-	}
-
 	/* Get the data length */
 	contentLength = article.getArticleSize();
 
